69aa3624fcb62084fd3f9965a481e43c05bea5b7
Bryan Hunwardsen <bryan@hunwardsen.com>
2013-02-15 11:53:38 -0800

    Added SImple Servlet

---
+++ MavenWebApp/src/main/java/com/hunwardsen/maven/webapp/SimpleServlet.java
@@ -0,0 +1,21 @@
+package com.hunwardsen.maven.webapp;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class SimpleServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		out.println("SimpleServlet Executed");
+		out.flush();
+		out.close();
+		// http://localhost:8080/MavenWebApp/simple
+	}
+}
